33243e9855634665efedf93354da2eff33f9e04a
Meow Man <ahmadmeshaal03@gmail.com>
2026-09-25 13:53:35 +0300

    end of 9

--- 9.js
+++ 9.js
@@ -3,8 +3,13 @@ let rtext="";
 var y=0;
 
 for(let i=0;i<text.length;i++){
-    if(text[y]==" "){
-        y++;
-        textk
+    while(text[i]==" "){
+        i++;
+    }
+    if(i==0 || text[i-1]==" " && text[i].charCodeAt(0)>=97 && text[i].charCodeAt(0)<=122){
+    rtext+=String.fromCharCode(text[i].charCodeAt(0)-32)
+    }else{
+    rtext+=text[i];
     }
 }
+alert(rtext)
